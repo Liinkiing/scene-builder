439fb110b7dbb3639337388c97bb03555e4d20b0
Omar Jbara <omar.Jbara2@gmail.com>
2017-12-30 01:59:12 +0100

    Deploy to Heroku

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ let express = require('express');
 let path = require('path');
 let serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/dist", {
+  maxAge: '1d'
+}));
 let port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
